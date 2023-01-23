893b4217b59b330f282aa7d4e5c1a716b8e72271
basilisgiata <basilisgiata@gmail.com>
2023-01-23 19:15:25 +0200

    app has now full authentication functionality and i tweaked some sccs code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 //= require jquery
+//= require jquery_ujs
+//= require bootstrap/dropdown
 //= require bootstrap-sprockets
+//= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
 
 root to: 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,4 +9,8 @@ root to: 'pages#index'
   devise_scope :user do
   get 'login', to: 'devise/sessions#new'
 end
+devise_scope :user do
+  get 'signup', to: 'devise/registrations#new'
+end
+
 end
