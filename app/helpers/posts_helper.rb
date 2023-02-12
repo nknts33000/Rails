fc6f47530d5ec17c61e060153b2d4128acdd9992
basilis giata <basilisgiata@hotmail.com>
2023-02-12 18:00:09 +0200

    main page now has branches and added a dummy search bar

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,30 @@
 module PostsHelper
+  def create_new_post_partial_path
+    if user_signed_in?
+      'posts/branch/create_new_post/signed_in'
+    else
+      'posts/branch/create_new_post/not_signed_in'
+    end
+  end
+  def no_posts_partial_path
+    @posts.empty? ? 'posts/branch/no_posts' : 'shared/empty_partial'
+  end
+  def category_field_partial_path
+    if params[:category].present?
+      'posts/branch/search_form/category_field'
+    else
+      'shared/empty_partial'
+    end
+  end
+  def all_categories_button
+    puts("hi"+params[:category].blank?.to_s)
+    if params[:category].blank?
+      'posts/branch/categories/all_selected'
+    else
+      'posts/branch/categories/all_not_selected'
+    end
+  end
+  def post_format_partial_path
+    current_page?(root_path) ? 'posts/post/home_page' : 'posts/post/branch_page'
+  end
 end
