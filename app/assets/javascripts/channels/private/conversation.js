c53d7bf1458400ae81ca839142660206a4d31fb1
nknts33000 <nknts33000@gmail.com>
2023-02-17 14:01:59 +0200

    Make a private conversation window's new message form functional - Add an event handler inside the   javascripts/channels/private/conversation.js to trigger the submit button - Define a common behavior among conversation windows inside the   assets/javascripts/conversations/conversation.js - Define a send_message function on both, client and server, sides

--- app/assets/javascripts/channels/private/conversation.js
+++ app/assets/javascripts/channels/private/conversation.js
@@ -3,3 +3,14 @@ App.private_conversation = App.cable.subscriptions.create("Private::Conversation
     disconnected: function() {},
     received: function(data) {}
 });
+$(document).on('submit', '.send-private-message', function(e) {
+    e.preventDefault();
+    var values = $(this).serializeArray();
+    App.private_conversation.send_message(values);
+    $(this).trigger('reset');
+});
+send_message: function(message) {
+    return this.perform('send_message', {
+        message: message
+    });
+}
