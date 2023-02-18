d96e88eefa12b4987f401e0107145a206d11f057
nknts33000 <nknts33000@gmail.com>
2023-02-19 01:07:32 +0200

    Create a Contact model and write specs for it

--- app/assets/javascripts/channels/private/conversation.js
+++ app/assets/javascripts/channels/private/conversation.js
@@ -1,1 +1,6 @@
-
+$(document).on('submit', '.send-private-message', function(e) {
+    e.preventDefault();
+    var values = $(this).serializeArray();
+    App.private_conversation.send_message(values);
+    $(this).trigger('reset');
+});
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_13_195624) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_18_173944) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "branch"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
